af5b00f44c74b962f57f4e7ebb7514897609a9c2
David Duy Ngo <d_ngo6@u.pacific.edu>
2017-09-03 00:01:01 -0700

    Initial push
    Restaurant, Chef, and Customer Classes

---
+++ Assets/classes/Chef.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/************************************
+ * Chef Class
+ * Description: Contains the attributes of the individual chef of the restaurant.
+ * Attributes:
+ * Name
+ * Skill
+ * Speed 
+ * 
+ * **********************************/
+
+public class Chef : MonoBehaviour {
+	enum CHEF_TYPE {HEAD, NORMAL};
+	public GameObject masterObject;
+	public string name = "default";
+
+	public float salary = 2000;
+	public int expenseID = 0;
+
+	//This is the skillset of the chef out of 100
+	public float nigiri_skill = 20;
+	public float roll_skill = 20;
+	public float specialroll_skill = 20;
+
+	//This is the speed of the sushi coming out.
+	public float nigiri_speed = 1; //countdown in seconds
+	public float roll_speed = 2;
+	public float specialroll_speed = 4;
+
+	void Start() {
+		expenseID = masterObject.GetComponent<Restaurant> ().addExpense (salary);
+	}
+
+}
---
+++ Assets/classes/Restaurant.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/****************************
+ * Restaurant Class
+ * Description: Attributes of the restaurant.
+ * Attributes:
+ * Name
+ * Days
+ * Money
+ * 
+ * Rent
+ * Expenses (Array)
+ * 
+ * Beginning of the game, the player starts off with a small loan of 10,000 dollars.
+ * 
+ * **************************/
+
+public class Restaurant : MonoBehaviour {
+	public static bool running = false;
+	//All attrivutes are public for debugging purposes.
+	public string restaurantName = "default";
+	public float day = 1;
+	public float money = 10000;
+	public float SECONDSINADAY = 90; //a simulated day is 90 seconds.
+
+	public float timeLeft = 30;
+	public float rent = 2000;
+	public List<float> expenses = new List<float>();
+
+	public float spawnRate = 30;
+	public float reputation = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (running == true) {
+			//Within this period of time, populate customers with orders.
+			if (UnityEngine.Random.Range (0, 100) <= spawnRate) {
+				money += 2; //simulate a customer buying an item.
+			}
+
+			timeLeft -= Time.deltaTime;
+			if (timeLeft <= 0) {
+				timeLeft = SECONDSINADAY;
+				running = false;
+
+				if (day % 2 == 0) {
+					//pay for all expenses
+					money -= rent;
+					foreach (float price in expenses) {
+						money -= price;
+					}
+				}
+					
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.X)) {
+			if (!running)
+				runDay();
+		}
+	}
+
+	/*
+	 * Simulates a whole day.
+	 */
+	public void runDay () {
+		running = true;
+		day++;
+	}
+
+	public int addExpense (float price) {
+		int expenseId = expenses.Count;
+
+		expenses.Add(price);
+
+		return expenseId;
+	}
+}
